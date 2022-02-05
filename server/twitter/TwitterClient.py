f2aae7e4df3cd6279dd9883771369f0152ca6e7f
jg <57115019+joeyagreco@users.noreply.github.com>
2022-02-05 14:01:34 -0600

    made fields private

--- server/twitter/TwitterClient.py
+++ server/twitter/TwitterClient.py
@@ -5,16 +5,16 @@ from server.auth.TwitterAuth import TwitterAuth
 
 
 class TwitterClient:
-    bearerToken = TwitterAuth.bearerToken
-    apiKey = TwitterAuth.apiKey
-    apiKeySecret = TwitterAuth.apiKeySecret
-    accessToken = TwitterAuth.accessToken
-    accessTokenSecret = TwitterAuth.accessTokenSecret
+    __bearerToken = TwitterAuth.bearerToken
+    __apiKey = TwitterAuth.apiKey
+    __apiKeySecret = TwitterAuth.apiKeySecret
+    __accessToken = TwitterAuth.accessToken
+    __accessTokenSecret = TwitterAuth.accessTokenSecret
 
     @classmethod
     def getClient(cls) -> Client:
-        return tweepy.Client(bearer_token=cls.bearerToken,
-                             consumer_key=cls.apiKey,
-                             consumer_secret=cls.apiKeySecret,
-                             access_token=cls.accessToken,
-                             access_token_secret=cls.accessTokenSecret)
+        return tweepy.Client(bearer_token=cls.__bearerToken,
+                             consumer_key=cls.__apiKey,
+                             consumer_secret=cls.__apiKeySecret,
+                             access_token=cls.__accessToken,
+                             access_token_secret=cls.__accessTokenSecret)
