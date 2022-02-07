2e5ee6700d11d4bcb39893515de605d73ff3615c
jg <57115019+joeyagreco@users.noreply.github.com>
2022-02-06 23:31:12 -0600

    cleanup

--- app.py
+++ app.py
@@ -3,9 +3,16 @@ from services.RatioService import RatioService
 """
 General Guide: https://dev.to/twitterdev/a-comprehensive-guide-for-using-the-twitter-api-v2-using-tweepy-in-python-15d9#:~:text=Tweepy%20is%20a%20popular%20package,the%20academic%20research%20product%20track.
 Search Queries: https://github.com/twitterdev/getting-started-with-the-twitter-api-v2-for-academic-research/blob/main/modules/5-how-to-write-search-queries.md
-"conversation_id field is always the Tweet ID of the original Tweet in the conversation reply thread."
+Get Tweet By ID UI: https://www.bram.us/2017/11/22/accessing-a-tweet-using-only-its-id-and-without-the-twitter-api/
 """
 
 if __name__ == "__main__":
-    service = RatioService()
-    service.harvestRatioReplies(10)
+    ratioService = RatioService()
+
+    # harvest tweets as long as we can
+    numberOfTweetsToHarvest = 100
+    numberOfResultsToServe = 100
+    while ratioService.harvestRatioReplies(numberOfTweetsToHarvest):
+        ratioService.harvestRatioReplies(numberOfTweetsToHarvest)
+    while True:
+        ratioService.serveRatioResults(numberOfResultsToServe)
