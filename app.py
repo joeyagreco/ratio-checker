9e33df2e9e82171e475fb383e52c6e059f6ad05e
jg <57115019+joeyagreco@users.noreply.github.com>
2022-02-06 23:31:40 -0600

    cleanup

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from services.RatioService import RatioService
 General Guide: https://dev.to/twitterdev/a-comprehensive-guide-for-using-the-twitter-api-v2-using-tweepy-in-python-15d9#:~:text=Tweepy%20is%20a%20popular%20package,the%20academic%20research%20product%20track.
 Search Queries: https://github.com/twitterdev/getting-started-with-the-twitter-api-v2-for-academic-research/blob/main/modules/5-how-to-write-search-queries.md
 Get Tweet By ID UI: https://www.bram.us/2017/11/22/accessing-a-tweet-using-only-its-id-and-without-the-twitter-api/
+API Request Limits: https://developer.twitter.com/en/docs/twitter-api/getting-started/about-twitter-api
 """
 
 if __name__ == "__main__":
